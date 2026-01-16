f54139295a55235791cafa2f68ba7018d3ae01e5
Double_K <konstantinkolesnikov999@gmail.com>
2026-01-16 17:28:30 +0200

    Revert "Merge pull request #7 from Kosta-zozo/local"
    This reverts commit 35ad29b1c3476202a8b3653918b7b21a4d6e3f93, reversing
    changes made to bfd6fd6faff89b5691f92e2fe691e7e0e890d3e7.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="col-lg-6 mx-auto">
         <p class="lead mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec consectetur lectus. Duis non pulvinar est. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla mattis ipsum ac lectus tempus, eget posuere massa placerat. Nulla facilisis molestie leo at tristique.</p>
         <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-            <a href="/gameHub" type="button" class="btn btn-primary btn-lg px-4 gap-3">Start game</a>
+            <a href="/game" type="button" class="btn btn-primary btn-lg px-4 gap-3">Start game</a>
         </div>
     </div>
 </div>

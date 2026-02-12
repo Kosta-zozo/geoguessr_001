68fc065413647e45117e4b8f259c7cba80806e11
Double_K <konstantinkolesnikov999@gmail.com>
2026-02-12 20:07:13 +0200

    placelist added

--- resources/views/adminpanel.blade.php
+++ resources/views/adminpanel.blade.php
@@ -2,7 +2,10 @@
 @section ('content')
 <div class="px-4 py-5 my-5 text-center">
     <div class="col-lg-6 mx-auto">
-        <a href="/addNewPlace" class="btn btn-primary">Add a new place</a>
+        @if(Auth::user()->admin)
+            <a href="/addNewPlace" class="btn btn-primary">Add a new place</a>
+        @endif
+        <a href="/placelist" class="btn btn-primary">View all places</a>
     </div>
 </div>
 @endsection
---
+++ resources/views/placelist.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('content')
+
+@if ($errors->any())
+    <div class="alert alert-danger">
+        @foreach ($errors->all() as $error)
+            <p>{{ $error }}</p>
+        @endforeach
+    </div>
+@endif
+
+<div class="row justify-content-center">
+    <div class="col-6">
+        @if(Auth::user()->admin)
+            <a href="/addNewPlace" class="btn btn-primary border-dark rounded-0 m-1">Add a new place</a>
+        @endif
+        @foreach ($places as $place)
+            <div class="row align-content-start border border-2 border-dark m-2 p-2">
+                <div class="col-3">
+                    <img src="img/{{ $place['image_name'] }}" alt="No image found" class="w-100 h-100 border border-1 border-dark">
+                </div>
+                <div class="col">
+                    <p>Image name: {{ $place['image_name'] }}</p>
+                    <p>Country: {{ $place['name'] }}</p>
+                    <p>Difficulty: {{ $place['difficulty'] }}</p>
+                    @if(Auth::user()->admin)
+                        <a href="/{{ $place['place_id'] }}/deleteplace" class="btn btn-danger border-dark rounded-0">Delete</a>
+                    @endif
+                </div>
+            </div>
+        @endforeach
+    </div>
+</div>
+
+@endsection

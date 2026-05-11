1437a70050cace48bf3b7e58a72cebd14427504c
Double_K <konstantinkolesnikov999@gmail.com>
2026-05-11 19:54:18 +0300

    Remove placeholder text from home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
         <span lang="lv">Geolokaciju minetajs</span>
     </h1>
     <div class="col-lg-6 mx-auto">
-        <p class="lead mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec consectetur lectus. Duis non pulvinar est. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla mattis ipsum ac lectus tempus, eget posuere massa placerat. Nulla facilisis molestie leo at tristique.</p>
+        <!-- <p class="lead mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec consectetur lectus. Duis non pulvinar est. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla mattis ipsum ac lectus tempus, eget posuere massa placerat. Nulla facilisis molestie leo at tristique.</p> -->
         <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
             <a href="/gameHub" type="button" class="btn btn-primary btn-lg px-4 gap-3 rounded-0" style="background-color: #46769B; border: 0;">
                 <span lang="en">Start game</span>
